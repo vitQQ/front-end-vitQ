9e47775afe8c64bfa536017efcdaad3fd8f9900b
diarih <diarihza@gmail.com>
2021-12-30 01:07:37 +0700

    add custom.scss and react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <div>
+    <div className="container fs-h3 fw-bold text-primary-3">
       Welcome Page
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import './styles/custom.scss'
+import './styles/index.css'
 // import Routes from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";

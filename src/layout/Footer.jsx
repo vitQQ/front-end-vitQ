35db6b5dc6283c4958e778c82da0ec2d01a71cf9
diarih <diarihza@gmail.com>
2021-12-30 18:34:25 +0700

    Edit scss, add footer

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "./styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="mt-auto bg-primary-1 fs-subtitle">
+      <div className="py-5 container text-active">
+        <Row className="mb-3 mb-sm-5 footerLink">
+          <Col sm={4} lg={4} md={4} xs={6}>
+            <Row className="gy-3">
+              <Col href="#" sm={12}>
+                <a href="https://www.dancommunity.co.id/id">Tentang Kami</a>
+              </Col>
+              <Col sm={12}>
+                <a href="https://www.dancommunity.co.id/id">Syarat dan Ketentuan</a>
+              </Col>
+              <Col sm={12}>
+                <a href="https://www.dancommunity.co.id/id">Privacy and Policy</a>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={4} lg={5} md={4} xs={6}>
+            <Row className="gy-3">
+              <Col sm={12}>
+                <a href="https://www.dancommunity.co.id/id">Bantuan</a>
+              </Col>
+              <Col sm={12}>
+                <a href="https://www.dancommunity.co.id/id">Kontak</a>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={4} lg={3} md={4} className="mt-1 mt-sm-4 text-center text-inactive">
+            Copyright &copy; 2022 VitQ
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}

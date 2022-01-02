7f08d201454853a3a1200835752bfcdc297b2d83
choconutzy <aprilianissa112@gmail.com>
2022-01-02 23:44:56 +0700

    add button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Button from "./components/button";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <div className="container fs-h3 fw-bold text-primary-3">
-      Welcome Page
+      <Footer/>
+      <Button/>
     </div>
   );
 }

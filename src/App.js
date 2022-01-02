675e00e554cbb48ee97aec140e94d12929374a53
choconutzy <aprilianissa112@gmail.com>
2022-01-03 01:11:32 +0700

    remove trial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Button from "./components/button";
-import Footer from "./layout/Footer";
 
 function App() {
   return (
     <div className="container fs-h3 fw-bold text-primary-3">
-      <Footer/>
-      <Button/>
     </div>
   );
 }

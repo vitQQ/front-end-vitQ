65c5f2a579cbd69b3b5c44e6b6cd87a909523561
choconutzy <aprilianissa112@gmail.com>
2022-01-04 11:32:58 +0700

    add props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./components/button";
 
-function App() {
+function App(props) {
   return (
     <div className="container fs-h3 fw-bold text-primary-3">
+      <Button value={`Ini Button`}/>
+      <Button disabled value={`Ini Button`}/>
+      <Button nofill value={`Ini Button`}/>
     </div>
   );
 }

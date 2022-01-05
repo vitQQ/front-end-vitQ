03075ff49c7e652839683ba24815ee6a7868bffe
dewimardanic <dewimardanic@gmail.com>
2022-01-05 08:22:39 +0800

    add landing pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate  } from "react-router-dom";
+import LandingPage from "../pages/LandingPage/LandingPage"
 
 export default function Routers() {
     return (
         <Routes>
-            <Route exact path="/" element={""} />
+            <Route exact path="/" element={<LandingPage />} />
             <Route exact path="*" element={<Navigate replace to="/" />} />
         </Routes>
     )

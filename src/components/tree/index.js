dba2ad3fd93ff0edb9524abb49c9d93e38d54194
diarih <diarihza@gmail.com>
2022-01-09 15:32:37 +0700

    Adding Home Pages and its assets components, Edit Route for Home, Edit Footer, Edit Layout

---
+++ src/components/tree/index.js
@@ -0,0 +1,8 @@
+const TREE = {
+  good: require("./Condition=Good.png"),
+  bad: require("./Condition=Bad.png"),
+  ok: require("./Condition=Ok.png"),
+  terrible: require("./Condition=Terrible.png"),
+};
+
+export default TREE;

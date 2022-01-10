a8385b2906958fc467819f97486d892e0ce3aa44
dewimardanic <dewimardanic@gmail.com>
2022-01-10 19:34:45 +0800

    add pages menu kalkulasi

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { Routes, Route} from "react-router-dom";
 import Home from "../pages/Home/Home";
-import Kalkulasi from "../pages/Kalkulasi/Kalkulasi";
 import Akunku from "../pages/Profil/Akunku";
 import NotFound from "../pages/NotFound";
 import LandingPage from "../pages/LandingPage/LandingPage"
 import Login from "../pages/login";
 import Daftar from "../pages/register";
 import HasilKalkulasi from "../pages/Kalkulasi/HasilKalkulasi";
+import MenuKalkululasi from "../pages/MenuKalkulasi/MenuKalkulasi";
 
 
 export default function AppRoutes() {
@@ -14,11 +14,12 @@ export default function AppRoutes() {
     <Routes className="container fs-h3 text-primary-3">
       <Route exact path="/" element={<LandingPage />} />
       <Route exact path="home" element={<Home />} />
-      <Route exact path="kalkulasi" element={<Kalkulasi />} />
+      {/* <Route exact path="kalkulasi" element={<Kalkulasi />} /> */}
       <Route exact path="kalkulasi/hasilkalkulasi" element={<HasilKalkulasi />} />
       <Route exact path="akunku" element={<Akunku />} />
       <Route exact path="/masuk" element={<Login />} />
       <Route exact path="/daftar" element={<Daftar />} />
+      <Route exact path="/kalkulasi" element={<MenuKalkululasi />} />
       <Route exact path="*" element={<NotFound />} />
     </Routes>
   );

8fe0d00a1f0536a3a7d9d661e2159b1f9cff26bf
choconutzy <aprilianissa112@gmail.com>
2022-01-11 14:34:49 +0700

    fix layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Footer from  "./Footer"
 import Navbar from "./Navbar"
+import useWindowDimensions from '../helpers/WindowDimension';
+import {useLocation} from 'react-router-dom'
 
 export default function Layout({children}) {
+    let {pathname} = useLocation()
+    let { width } = useWindowDimensions()
     return (
         <div className="d-flex flex-column" style={{height: "100vh"}}>
             <Navbar/>
                 {children}
-            <Footer/>
+            {width>768?<Footer/>
+            : pathname === '/' | pathname === '/home' ? <Footer/> :
+            <div className={pathname!=='/masuk' | pathname!=='/daftar'?"space text-black-50" : "d-none"}>
+                <p className="text-white">Footer d-none</p>
+            </div>
+}
         </div>
     )
 }
